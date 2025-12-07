c46600c1645556baa481b7568cc0caa1bc0fe339
三眼怪 <Sx508375@gmail.com>
2025-12-07 18:37:41 +0800

    Update game scripts for Plants vs Brainrot

--- Loader.lua
+++ Loader.lua
@@ -4,7 +4,7 @@ if setfpscap then setfpscap(240) end
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Nash29197/AlienHub/refs/heads/main/%25%25%25%25%25"))()
 
 if game.PlaceId == 127742093697776 then ---Plants vs Brainrot
-    loadstring(game:HttpGet("https://scriptprotector.vercel.app/api/raw/fb7424dd3c0489db62fbab92adef599c"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Nash29197/AlienHub/refs/heads/main/PvB-Plants%20Vs%20Brainrot"))()
 if game.PlaceId == 136801880565837 then ---[FPS]Flick
     loadstring(game:HttpGet("https://scriptprotector.vercel.app/api/raw/44135305438be3b1edbb6bca6d9fe088"))()
 if game.PlaceId == 126884695634066 then ---GROW A GARDEN
