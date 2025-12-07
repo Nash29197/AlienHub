f450c3c9335377f0cb3882a1c176cced7ff0bbc1
三眼怪 <Sx508375@gmail.com>
2025-12-07 18:39:33 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -6,7 +6,7 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/Nash29197/AlienHub/re
 if game.PlaceId == 127742093697776 then ---Plants vs Brainrot
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Nash29197/AlienHub/refs/heads/main/PvB-Plants%20Vs%20Brainrot"))()
 if game.PlaceId == 136801880565837 then ---[FPS]Flick
-    loadstring(game:HttpGet("https://scriptprotector.vercel.app/api/raw/44135305438be3b1edbb6bca6d9fe088"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gibssss-art/vuln/refs/heads/main/Flicker"))()
 if game.PlaceId == 126884695634066 then ---GROW A GARDEN
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Gibssss-art/vuln/refs/heads/main/GrowAFUCKER"))()
 end
