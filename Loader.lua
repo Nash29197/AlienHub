5e3320ceadf2de3df7edd6203b00e1ef7f90ca20
三眼怪 <Sx508375@gmail.com>
2025-12-07 18:57:03 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,12 +1,15 @@
 repeat task.wait() until game:IsLoaded()
 if setfpscap then setfpscap(240) end
 
+local loaders = {
+    [127742093697776] = "https://raw.githubusercontent.com/Nash29197/AlienHub/refs/heads/main/PvB-Plants%20Vs%20Brainrot",
+    [136801880565837] = "https://raw.githubusercontent.com/Gibssss-art/vuln/refs/heads/main/Flicker",
+    [126884695634066] = "https://raw.githubusercontent.com/Gibssss-art/vuln/refs/heads/main/GrowAFUCKER",
+}
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Nash29197/AlienHub/refs/heads/main/%25%25%25%25%25"))()
 
-if game.PlaceId == 127742093697776 then ---Plants vs Brainrot
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Nash29197/AlienHub/refs/heads/main/PvB-Plants%20Vs%20Brainrot"))()
-if game.PlaceId == 136801880565837 then ---[FPS]Flick
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gibssss-art/vuln/refs/heads/main/Flicker"))()
-if game.PlaceId == 126884695634066 then ---GROW A GARDEN
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gibssss-art/vuln/refs/heads/main/GrowAFUCKER"))()
+local url = loaders[game.PlaceId]
+if url then
+    loadstring(game:HttpGet(url))()
 end
